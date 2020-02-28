3c6ec50b0d6c952481ab03def4bec093c5fec91e
Omar Alaa <omar.skillful@gmail.com>
2020-02-28 15:31:02 +0200

    Finished courses HTTP verbs - fixed post bugs - restructured validation to model

--- routes/courses.js
+++ routes/courses.js
@@ -1,22 +1,57 @@
 const express = require('express');
 const _ = require('lodash');
-const Joi = require('@hapi/joi');
 
 const router = express.Router();
 
 const Course = require('../models/course');
 const courseService = require('../services/courseService');
+const {isValidObjectId} = require('mongoose');
 
 router.get('/', async (req, res) => {
     const courses = await courseService.getCourses();
     res.send(courses);
 });
 
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    if (!isValidObjectId(id)) return res.status(400).send('Invalid ID');
+
+    const course = await courseService.getCourse(id);
+    if (!course) return res.status(404).send('No course with this ID is found.');
+
+    res.send(course);
+});
+
 router.post('/', async (req, res) => {
     const {error} = Course.validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-    const course = new Course(_.pick(req.body, ['name', 'creditHours', 'courseType', 'term']));
+    let course = new Course(_.pick(req.body, ['name', 'creditHours', 'courseType', 'term']));
     course = await courseService.saveCourse(course);
+    res.status(201).send(course);
+});
+
+router.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    if (!isValidObjectId(id)) return res.status(400).send('Invalid ID');
+
+    const {error} = Course.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);  
+
+
+    // TODO: CHECK IF THERE IS A WAY TO SEE IF THE ERROR IS NOT ID ONLY INVOLVED
+    const course = await courseService.updateCourse(id, body);
+    if (!course) return res.status(404).send('No course with this ID is found.');
+
+    res.send(course);
+});
+
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+    if (!isValidObjectId(id)) return res.status(400).send('Invalid ID');
+
+    const course = await courseService.deleteCourse(id);
+    if (!course) return res.status(404).send('No course with this ID is found.');
+
     res.send(course);
 });
 
--- services/courseService.js
+++ services/courseService.js
@@ -7,12 +7,30 @@ const getCourses = async () => {
     return courses;
 };
 
+const getCourse = async (id) => {
+    const course = await Course.findById(id);
+    return course;
+}
+
 const saveCourse = async (course) => {
     course = await course.save();
     return course;
 };
 
+const updateCourse = async (id, body) => {
+    const course = Course.findByIdAndUpdate(id, body, {new: true});
+    return course;
+}
+
+const deleteCourse = async (id) => {
+    const course = await Course.findByIdAndRemove(id);
+    return course;
+}
+
 module.exports = {
     getCourses,
-    saveCourse
+    getCourse,
+    saveCourse,
+    updateCourse,
+    deleteCourse
 }
